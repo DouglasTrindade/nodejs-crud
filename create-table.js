ed61500f62e5913e9047b64425ca748a7a0b2e4e
Douglas Trindade <douglastrindade22@hotmail.com>
2024-04-15 02:18:04 -0300

    api node postgres

---
+++ create-table.js
@@ -0,0 +1,16 @@
+import { sql } from "./neondb.js";
+
+// sql`DROP TABLE IF EXISTS videos;`.then(() => {
+//   console.log("Tabela deletada.");
+// });
+
+sql`
+CREATE TABLE videos (
+  id TEXT PRIMARY KEY,
+  title VARCHAR(255),
+  description TEXT,
+  duration INTEGER
+);
+`.then(() => {
+  console.log("Tabela criada!");
+});
